7fe656c1a267adbf32e30fb1ea65e0d1a8f72be3
TheMekanik <2210512064@mahasiswa.upnvj.ac.id>
2025-01-13 14:45:38 +0700

    first commit

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Notifications\Notifiable;
+
+class ProfileController extends Controller
+{
+    use Notifiable, HasFactory;
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function show_profile()
+    {
+        $user = Auth::user();
+        return view('show_profile', compact('user'));
+    }
+
+    public function edit_profile(Request $request)
+    {
+        $request->validate([
+            'name' => 'required', 
+            'password' => 'required|min:8|confirmed'
+        ]);
+
+        $user = Auth::user();
+        $user->update([
+            'name' => $request->name, 
+            'password' => Hash::make($request->password)
+        ]);
+
+        return Redirect::back();
+    }
+}
